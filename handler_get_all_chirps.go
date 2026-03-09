306bfde93e8b5746d1406bb5e1ea8bd4379ddff1
SamW-hash <samvincentwilliams@gmail.com>
2026-03-08 18:03:46 -0700

    created get_all_chirps handler

---
+++ handler_get_all_chirps.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
+	chirps, err := cfg.db.GetAllChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+	}
+
+	type chirpResponse struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body      string    `json:"body"`
+		UserID    uuid.UUID `json:"user_id"`
+	}
+	respBody := make([]chirpResponse, len(chirps))
+	for i, c := range chirps {
+		respBody[i] = chirpResponse{
+			ID:        c.ID,
+			CreatedAt: c.CreatedAt,
+			UpdatedAt: c.UpdatedAt,
+			Body:      c.Body,
+			UserID:    c.UserID,
+		}
+	}
+
+	respondWithJSON(w, 200, respBody)
+}
